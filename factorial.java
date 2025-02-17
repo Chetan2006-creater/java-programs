9f96b4bc4f4b12a1f77be3953813f2fc90ef8f13
Chetan2006-creater <dchetansivapavankumar@gmail.com>
2025-02-17 11:07:45 +0530

    Add files via upload

---
+++ factorial.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class factorial{
+public static void main(String[] args){
+Scanner read = new Scanner(System.in);
+System.out.print("Enter the positive number ");
+int n = read.nextInt();
+int fac = 1;
+for(int i=n;i>=1;i--){
+fac = fac*i ;
+}
+System.out.print("factorial of the numberis = "+fac);
+}
+}
