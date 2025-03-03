273db26e8bb060bbf9b2f2364c47b64f17718b4f
Chetan2006-creater <dchetansivapavankumar@gmail.com>
2025-03-03 12:22:34 +0530

    Update book.java

--- book.java
+++ book.java
@@ -7,13 +7,15 @@ this.Title = Title;
 this.Author = Author;
 this.Year_of_publication = Year_of_publication;
 }
-public static void display(){
-System.out.println("Title of the book is "+ Title +", Its Author is "+ Author +", Its year of Publication is "+ Year_of_publication);
+public void display(){
+System.out.println("Title of the book is "+ Title +'\n'+"Its Author is "+ Author +'\n'+"Its year of Publication is "+ Year_of_publication);
 }
 public static void main(String[] args){
 book book1 = new book("Thomus calculus" , "Thomas" , 1999);
 book book2 = new book("Anandmath" , "Bankim Chandra Chatterjee" , 2006);
 book book3 = new book("Chandalika" , "Rabindra Nath Tagore" , 1937);
 book1.display();
+book2.display();
+book3.display();
 }
 }
