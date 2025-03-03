c43b6e8dff5bf66242bd60d921e397334775275e
Chetan2006-creater <dchetansivapavankumar@gmail.com>
2025-03-03 12:16:12 +0530

    Add files via upload

---
+++ book.java
@@ -0,0 +1,19 @@
+public class book{
+String Title;
+String Author;
+int Year_of_publication;
+public book(String Title, String Author, int Year_of_publication){
+this.Title = Title;
+this.Author = Author;
+this.Year_of_publication = Year_of_publication;
+}
+public static void display(){
+System.out.println("Title of the book is "+ Title +", Its Author is "+ Author +", Its year of Publication is "+ Year_of_publication);
+}
+public static void main(String[] args){
+book book1 = new book("Thomus calculus" , "Thomas" , 1999);
+book book2 = new book("Anandmath" , "Bankim Chandra Chatterjee" , 2006);
+book book3 = new book("Chandalika" , "Rabindra Nath Tagore" , 1937);
+book1.display();
+}
+}
