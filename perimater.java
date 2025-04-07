a5cb96a3728185c3bbdf5a00feb56574918d9f54
Chetan2006-creater <dchetansivapavankumar@gmail.com>
2025-04-07 23:08:58 +0530

    Update perimater.java

--- perimater.java
+++ perimater.java
@@ -1,6 +1,6 @@
 // perimeter using interface concept
 interface Shape {
-	void getPerimeter()
+	void getPerimeter();
 }
 class Rectangle implements Shape{
 	int length ;
@@ -10,23 +10,23 @@ class Rectangle implements Shape{
 	this.width = width ;
 	}
 	public void getPerimeter(){
-		System.out.println("Perimeter of the rectangle = "+ 2(length + width));
+		System.out.println("Perimeter of the rectangle = "+ 2*(length + width));
 	}
 }
 class Circle implements Shape{
 	int Radius ;
-	Rectangle(int Radius){
+	Circle(int Radius){
 	this.Radius = Radius ;
 	}
 	public void getPerimeter(){
-		System.out.println("Perimeter of the Circle = "+ 2*3.14*Radius);
+		System.out.println("Perimeter of the Circle = "+ (2*3.14*Radius));
 	}
 }
 class Triangle implements Shape{
 	int a ;
 	int b ;
 	int c ;
-	Rectangle(int a, int b , int c){
+	Triangle(int a, int b , int c){
 	this.a = a  ;
 	this.b = b  ;
 	this.c = c  ;
@@ -38,5 +38,12 @@ class Triangle implements Shape{
 class main{
 	public static void main(String[] args){
 	Rectangle r = new Rectangle(5,10);
+	Circle c = new Circle (7);
+	Triangle t = new Triangle (5,6,3);
+	r.getPerimeter();
+	System.out.println();
+	c.getPerimeter();
+	System.out.println();
+	t.getPerimeter();
 	}
 }
